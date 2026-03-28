6c960e284d39d2f0c5a94c8a3a9c0760389e2e33
Santiago Moreno <Santiago.moreno@usantoto.edu.co>
2026-03-27 21:27:09 -0500

    Render testing

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+const requestLogger = (req, res, next) => {
+    console.log('Method: ', req.method);
+    console.log('Path: ', req.path);
+    console.log('Body: ', req.body);
+    console.log('---');
+    next()   
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+} 
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+    const type = request.get('content-type')
+    console.log(type);
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note =>  note.id === id)
+
+    if (note) {
+        res.json(note)
+    } else {
+        res.statusMessage = "There's an non existant id in the link"
+        res.status(404).end()
+    }
+})
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+
+    if (!body.content) {
+        return response.status(404).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.statusMessage = "Key: "+ id + " deleted successfully"
+    res.status(204).end()
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
