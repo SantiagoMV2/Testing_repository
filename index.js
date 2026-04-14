aad2fd7ec184462ca31b6ceb1d068602c76f9c5e
Santiago Moreno <Santiago.moreno@usantoto.edu.co>
2026-04-14 13:20:01 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
+const Note = require('./models/note')
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const requestLogger = (req, res, next) => {
     console.log('Method: ', req.method);
@@ -33,58 +29,52 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
-} 
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-    const type = request.get('content-type')
-    console.log(type);
+app.get('/api/notes', (req, res) => {
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note =>  note.id === id)
-
-    if (note) {
-        res.json(note)
-    } else {
-        res.statusMessage = "There's an non existant id in the link"
-        res.status(404).end()
-    }
+    Note.findById(req.params.id)
+    .then(note => {
+        if (note) {
+            res.json(note)
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => res.status(400).send({ error: 'malformatted id'}))
 })
 
 app.post('/api/notes', (req, res) => {
     const body = req.body
 
     if (!body.content) {
-        return response.status(404).json({
+        return res.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-    res.json(note)
+    note.save().then(savedNote => {
+        res.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.statusMessage = "Key: "+ id + " deleted successfully"
-    res.status(204).end()
+    Note.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.status(204).end()
+    })
 })
 
 const unknownEndpoint = (req, res) => {
